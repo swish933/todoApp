c9c9703ddada2110f6a348bb394e66e0c3366b71
Ikem Nomso <ikemnomso@gmail.com>
2021-05-14 12:26:15 +0100

    change theme onclick

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import styles from './Header.module.css';
-// import moon from '../images/icon-moon.svg';
+import moon from '../images/icon-moon.svg';
 import sun from '../images/icon-sun.svg';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { THEME } from '../constants';
+import { setTheme } from '../actions/actions';
 
-const Header = () => {
+const Header = ({ changeTheme, theme, setTheme }) => {
+	const onClick = () => {
+		switch (theme) {
+			case THEME.LIGHT:
+				setTheme(THEME.DARK);
+				break;
+			case THEME.DARK:
+				setTheme(THEME.LIGHT);
+				break;
+			default:
+				return;
+		}
+	};
+
 	return (
 		<div className={styles.Header}>
 			<h1>TODO</h1>
-			<img src={sun} alt='Theme-Icon' />
+			<img
+				src={theme === 'darkMode' ? sun : moon}
+				alt='Theme-Icon'
+				onClick={onClick}
+			/>
 		</div>
 	);
 };
 
-export default Header;
+Header.propTypes = {
+	theme: PropTypes.string.isRequired,
+	setTheme: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+	theme: state.theme,
+});
+export default connect(mapStateToProps, { setTheme })(Header);
