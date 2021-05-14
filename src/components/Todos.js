a0732ddc232c61d2150974428f3130832a27995d
Ikem Nomso <ikemnomso@gmail.com>
2021-05-14 14:27:41 +0100

    styles fixes

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,8 +1,11 @@
+import { Fragment } from 'react';
 import Todo from './Todo';
+import Menu from './Menu';
+import styles from './Todos.module.css';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import styles from './Todos.module.css';
-import { VISIBILITY_FILTERS } from '../constants';
+import { VISIBILITY_FILTERS, THEME } from '../constants';
+import cx from 'classnames';
 
 const getTodobyFilter = (todos, filter) => {
 	switch (filter) {
@@ -15,31 +18,40 @@ const getTodobyFilter = (todos, filter) => {
 	}
 };
 
-const Todos = ({ todos, filter }) => {
+const Todos = ({ todos, filter, theme }) => {
 	return (
-		<div className={styles.Todos}>
-			{getTodobyFilter(todos, filter).map((todo) => {
-				return (
-					<Todo
-						key={todo.id}
-						content={todo.content}
-						id={todo.id}
-						completed={todo.completed}
-					/>
-				);
-			})}
-		</div>
+		<Fragment>
+			<div
+				className={cx(styles.Todos, {
+					[styles.darkMode]: theme === THEME.DARK,
+					[styles.lightMode]: theme === THEME.LIGHT,
+				})}>
+				{getTodobyFilter(todos, filter).map((todo) => {
+					return (
+						<Todo
+							key={todo.id}
+							content={todo.content}
+							id={todo.id}
+							completed={todo.completed}
+						/>
+					);
+				})}
+			</div>
+			<Menu theme={theme} todos={todos} filter={filter} />
+		</Fragment>
 	);
 };
 
 Todos.propTypes = {
 	todos: PropTypes.array.isRequired,
 	filter: PropTypes.string.isRequired,
+	theme: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
 	todos: state.todos,
 	filter: state.visibilityFilter,
+	theme: state.theme,
 });
 
 export default connect(mapStateToProps, null)(Todos);
